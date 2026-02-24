92976ded5356062fafdb72b4107c9fe0f17e3dda
Zichen Sui <2381943315@qq.com>
2026-02-24 14:12:25 +0800

    解决了“项目与任务不对齐”的bug

--- ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/chat/domain/TaskManagementFile.java
+++ ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/chat/domain/TaskManagementFile.java
@@ -65,7 +65,7 @@ public class TaskManagementFile implements Serializable {
     /**
      * 上传时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private Date uploadTime;
 
     /**
@@ -76,13 +76,13 @@ public class TaskManagementFile implements Serializable {
     /**
      * 创建时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private Date createTime;
 
     /**
      * 更新时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private Date updateTime;
 
     /**
